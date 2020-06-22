df2dfe90aaef3a76d2671fb4efb949f6d83a63b6
hackanda <yadavrishabh98@gmail.com>
2020-06-22 22:49:55 +0530

    Newer Version of Code

--- Image project.py
+++ Image project.py
@@ -6,9 +6,9 @@ import cv2 as cv
 import numpy as np
 
 # loading the face detection classifier
-face_cascade = cv.CascadeClassifier('readonly/haarcascade_frontalface_default.xml')
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-filename = "readonly/small_img.zip"
+filename = "small_img.zip"
 
 def readZipFile(file):
     with ZipFile(file , 'r') as myfile:
